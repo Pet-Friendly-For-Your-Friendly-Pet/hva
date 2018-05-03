b1f5d60d354c7023e678dd89905ac3054bd1b1a3
Pet-Friendly-For-Your-Friendly-Pet <tccarlson04@gmail.com>
2018-05-03 15:09:32 -0600

    refactored server

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({
-    hi: " : ) "
-  });
-});
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
