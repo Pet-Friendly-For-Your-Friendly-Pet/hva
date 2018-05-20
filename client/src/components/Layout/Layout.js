db2281689042ae597fa20bb2e69b7c48e615551c
Tom Carlson <tccarlson04@gmail.com>
2018-05-20 11:55:38 -0600

    about to do some responsive styling, so this is just in case I do something that cannot be undone

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -4,10 +4,12 @@ import classes from "./Layout.css";
 import AuxWrapper from "../../hoc/auxWrapper";
 import Services from "../../containers/Services/Services";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <AuxWrapper>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </AuxWrapper>
 );
