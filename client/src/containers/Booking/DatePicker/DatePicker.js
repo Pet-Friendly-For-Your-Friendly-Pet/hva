66f6da32b2e1576e155eceed019eea2a71fd1be7
Pet-Friendly-For-Your-Friendly-Pet <tccarlson04@gmail.com>
2018-05-31 13:43:54 -0600

    jesus h this booking is a blast :)

--- client/src/containers/Booking/DatePicker/DatePicker.js
+++ client/src/containers/Booking/DatePicker/DatePicker.js
@@ -56,57 +56,107 @@ class DatePicker extends Component {
         />
         <Helmet>
           <style>{`
+            .DayPicker-Months {
+              padding-left: 25px;
+            }
 
-          .DayPicker-Months {
-            padding-left: 25px;
-          }
+            .RangeExample p {
+              padding: 0;
+              margin: 0;
+              font-weight: lighter;
+                text-align: center;
+                justify-content: center;
+                letter-spacing: 5px;
+              }
 
-          .RangeExample p {
-            padding: 0;
-            margin: 0;
-            font-weight: lighter;
-            text-align: center;
-            justify-content: center;
-            letter-spacing: 5px;
-          }
+              .link {
+                color: black;
+                outline: none;
+                padding: 5px 10px;
+                margin-top: 2%;
+                font-size: 0.9em;
+                font-family: inherit;
+                background-color: transparent;
+                font-weight: lighter;
+                letter-spacing: 4px;
+                border-top: none;
+                border-right: none;
+                border-left: none;
+                transition: background-color 0.4s ease-in-out, color 0.4s ease-in-out;
+              }
 
-          .link {
-            color: black;
-            outline: none;
-            padding: 5px 10px;
-            margin-top: 2%;
-            font-size: 0.9em;
-            font-family: inherit;
-            background-color: transparent;
-            font-weight: lighter;
-            letter-spacing: 4px;
-            border-top: none;
-            border-right: none;
-            border-left: none;
-            transition: background-color 0.4s ease-in-out, color 0.4s ease-in-out;
-          }
+              .link:hover {
+                  color: white;
+                  cursor: pointer;
+                  background-color: rgb(218, 35, 35);
+              }
+              .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+                background-color: #f0f8ff !important;
+                color: #4a90e2;
+              }
+              .Selectable .DayPicker-Day {
+                border-radius: 0 !important;
+              }
+              .Selectable .DayPicker-Day--start {
+                border-top-left-radius: 50% !important;
+                border-bottom-left-radius: 50% !important;
+              }
+              .Selectable .DayPicker-Day--end {
+                border-top-right-radius: 50% !important;
+                border-bottom-right-radius: 50% !important;
+              }
 
-          .link:hover {
-              color: white;
-              cursor: pointer;
-              background-color: rgb(218, 35, 35);
-          }
-  .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
-    background-color: #f0f8ff !important;
-    color: #4a90e2;
-  }
-  .Selectable .DayPicker-Day {
-    border-radius: 0 !important;
-  }
-  .Selectable .DayPicker-Day--start {
-    border-top-left-radius: 50% !important;
-    border-bottom-left-radius: 50% !important;
-  }
-  .Selectable .DayPicker-Day--end {
-    border-top-right-radius: 50% !important;
-    border-bottom-right-radius: 50% !important;
-  }
-`}</style>
+              @media (min-device-width: 834px) {
+              .DayPicker-Months {
+                font-size: .9em;
+              }
+
+              .RangeExample p {
+                padding: 0;
+                margin: 0;
+                font-weight: lighter;
+                text-align: center;
+                justify-content: center;
+                letter-spacing: 5px;
+              }
+
+              .link {
+                color: black;
+                outline: none;
+                padding: 5px 10px;
+                margin-top: 2%;
+                font-size: 0.9em;
+                font-family: inherit;
+                background-color: transparent;
+                font-weight: lighter;
+                letter-spacing: 4px;
+                border-top: none;
+                border-right: none;
+                border-left: none;
+                transition: background-color 0.4s ease-in-out, color 0.4s ease-in-out;
+              }
+              .link:hover {
+                  color: white;
+                  cursor: pointer;
+                  background-color: rgb(218, 35, 35);
+              }
+              .Selectable .DayPicker-Day--selected:not(.DayPicker-Day--start):not(.DayPicker-Day--end):not(.DayPicker-Day--outside) {
+                background-color: #f0f8ff !important;
+                color: #4a90e2;
+              }
+              .Selectable .DayPicker-Day {
+                border-radius: 0 !important;
+              }
+              .Selectable .DayPicker-Day--start {
+                border-top-left-radius: 50% !important;
+                border-bottom-left-radius: 50% !important;
+              }
+              .Selectable .DayPicker-Day--end {
+                border-top-right-radius: 50% !important;
+                border-bottom-right-radius: 50% !important;
+              }
+            }
+        `}</style>
         </Helmet>
       </div>
     );
