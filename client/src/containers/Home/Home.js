0e328d8599fb244269ba32d79a3bb766ca8cc3a2
Tom Carlson <tccarlson04@gmail.com>
2018-06-17 18:56:45 -0600

    homepage content section addition

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-import classes from './Home.css';
+import React, { Component } from "react";
+import classes from "./Home.css";
 
-import Gallery from './Gallery/Gallery';
-import Footer from '../../components/Footer/Footer';
+import Gallery from "./Gallery/Gallery";
+import HomeContent from "./HomeContent/HomeContent";
+import Footer from "../../components/Footer/Footer";
 
 class Home extends Component {
-    state = {
-    }
-    //Add methods below, above render()
-    
-    render() {
-        return (
-            <div>
-                <Gallery />
-                <Footer />
-            </div> 
-        );
-    }
+  state = {};
+  //Add methods below, above render()
+
+  render() {
+    return (
+      <div className={classes.container}>
+        <main>
+          <Gallery />
+          <HomeContent />
+          <Footer />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default Home;
---
+++ client/src/containers/Home/HomeContent/HomeContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./HomeContent.css";
+
+const HomeContent = props => (
+  <div className={classes.container}>
+    <main>
+      <h1>Bar One</h1>
+    </main>
+  </div>
+);
+
+export default HomeContent;
